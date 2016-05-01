12dbb1bbdd5eac3508b2c341e86766a3971686ff
Atakan Goktepe <atakangktepe@gmail.com>
2016-05-01 13:01:37 +0300

    Created some files releated controller

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,29 @@
 class DrinksController < ApplicationController
+  def index
+    # Will have template
+  end
+
+  def show
+    # Will have template
+  end
+
+  def new # Display the for new record
+    # Will have template
+  end
+
+  def create # Save new record
+    # Will save and redirect
+  end
+
+  def edit # Display form for existing record
+    # Will have template
+  end
+
+  def update # Save changes
+    # Will save and redirect
+  end
+
+  def destroy
+    # Will destroy and redirect
+  end
 end
